8bb321c4fce3d294c981f2a98dd1debc25fa1558
Samir <Saitwalsamir@gmail.com>
2021-03-03 21:41:31 +0530

    --models added in Job Profile

--- Django_Intershala/Django_Intershala/settings.py
+++ Django_Intershala/Django_Intershala/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'job_profile',
     'recruiter',
     'student',
     'drf_yasg',
