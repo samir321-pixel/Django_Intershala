7d2d4b63f435c5e995a203792a6af896cdc90189
Samir <Saitwalsamir@gmail.com>
2021-03-04 21:28:13 +0530

    --search filter added

--- Django_Intershala/intershala_admin/views.py
+++ Django_Intershala/intershala_admin/views.py
@@ -5,6 +5,7 @@ from .serializers import *
 from student.models import Student
 from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.filters import SearchFilter
 
 
 class IntershalaStudentViewSets(generics.ListAPIView, generics.DestroyAPIView):
@@ -12,10 +13,13 @@ class IntershalaStudentViewSets(generics.ListAPIView, generics.DestroyAPIView):
     serializer_class = IntershalaStudentSerializer
     permission_classes = (IsAuthenticated,)
     lookup_field = "id"
+    filter_backends = [SearchFilter, ]
+    search_fields = ['first_name', 'email', 'date']
 
     def list(self, request, *args, **kwargs):
         if self.request.user.is_employee or self.request.user.is_superuser or self.request.user.is_admin:
             queryset = self.get_queryset()
+            queryset = self.filter_queryset(queryset)
             queryset = self.filter_queryset(queryset)
             serializer = self.get_serializer(queryset, many=True)
             return Response(serializer.data, status=200)
@@ -32,4 +36,3 @@ class IntershalaStudentViewSets(generics.ListAPIView, generics.DestroyAPIView):
             return Response({"Student Deleted": "Access Granted"}, status=200)
         else:
             return Response({"NO_ACCESS": "Access Denied"}, status=401)
-
