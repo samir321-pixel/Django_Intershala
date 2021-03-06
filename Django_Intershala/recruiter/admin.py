499e7283f8aa03070707fc98311daa285ed42ae2
Samir <Saitwalsamir@gmail.com>
2021-03-06 13:24:01 +0530

    --recruiter notification model added

--- Django_Intershala/recruiter/admin.py
+++ Django_Intershala/recruiter/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
-
+from .notification_models import *
 # Register your models here.
 admin.site.register(Recruiter)
+admin.site.register(RecruiterNotification)
