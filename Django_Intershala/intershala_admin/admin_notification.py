7ca75c70ea9129e6c3bd2375425184a5e17536ac
Samir <Saitwalsamir@gmail.com>
2021-03-08 22:37:45 +0530

    --notification updated

--- Django_Intershala/intershala_admin/admin_notification.py
+++ Django_Intershala/intershala_admin/admin_notification.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
 
 
-class AdminNotificationViewsets(generics.ListAPIView, generics.RetrieveAPIView):
+class AdminNotificationViewsets(generics.ListAPIView):
     serializer_class = AdminNotificationSerializer
     queryset = AdminNotification.objects.all().order_by('-created_at')
     permission_classes = (IsAuthenticated,)
@@ -19,8 +19,40 @@ class AdminNotificationViewsets(generics.ListAPIView, generics.RetrieveAPIView):
             serializer = self.get_serializer(queryset, many=True)
             return Response(serializer.data, status=200)
 
+
+class AdminNotificationRetriveViewsets(generics.RetrieveAPIView):
+    serializer_class = AdminNotificationSerializer
+    queryset = AdminNotification.objects.all().order_by('-created_at')
+    permission_classes = (IsAuthenticated,)
+
     def retrieve(self, request, *args, **kwargs):
         if self.request.user.is_admin or self.request.user.is_superuser or self.request.user.is_employee:
             queryset = AdminNotification.objects.get(id=self.kwargs["id"])
+            serializer = self.get_serializer(queryset)
+            return Response(serializer.data, status=200)
+
+
+class EmployeeNotificationViewsets(generics.ListAPIView, generics.RetrieveAPIView):
+    serializer_class = EmployeeNotificationSerializer
+    queryset = EmployeeNotification.objects.all().order_by('-created_at')
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_employee:
+            queryset = EmployeeNotification.objects.all().order_by('-created_at')
+            for i in queryset:
+                i.seen = True
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data, status=200)
+
+
+class EmployeeNotificationRetriveViewsets(generics.RetrieveAPIView):
+    serializer_class = EmployeeNotificationSerializer
+    queryset = EmployeeNotification.objects.all().order_by('-created_at')
+    permission_classes = (IsAuthenticated,)
+
+    def retrieve(self, request, *args, **kwargs):
+        if self.request.user.is_employee:
+            queryset = EmployeeNotification.objects.get(id=self.kwargs["id"])
             serializer = self.get_serializer(queryset)
             return Response(serializer.data, status=200)
