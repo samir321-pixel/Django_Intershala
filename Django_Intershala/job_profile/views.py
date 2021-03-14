c4fb41029adc1cf158c52a37562f53bd26002e16
Samir <Saitwalsamir@gmail.com>
2021-03-14 21:35:47 +0530

    --post job profile updated

--- Django_Intershala/job_profile/views.py
+++ Django_Intershala/job_profile/views.py
@@ -39,11 +39,11 @@ class ProfileViewSet(viewsets.ModelViewSet):
         try:
             if self.request.user.is_recruiter:
                 recruiter_query = Recruiter.objects.get(user=self.request.user.id)
-                company_query = IntershalaCompany.objects.get(id=recruiter_query.company.id)
                 if recruiter_query.active:
                     serializer = ProfileSerializer(data=self.request.data)
                     if serializer.is_valid(raise_exception=True):
-                        data = serializer.save(recruiter=recruiter_query, active=True, company=company_query)
+                        data = serializer.save(recruiter=recruiter_query, active=True,
+                                               company=IntershalaCompany.objects.get(id=recruiter_query.company.id))
                         recruiter_query = Recruiter.objects.get(user=self.request.user.id)
                         recruiter_query.created_profile.add(data)
                         return Response(serializer.data, status=200)
@@ -51,8 +51,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
                         return Response(serializer.errors, status=401)
                 elif not recruiter_query.active:
                     return Response({"NO_ACCESS": "Access Denied"}, status=401)
-        except Exception as e:
-            print(e)
+        except:
             return Response({"NO_ACCESS": "Access Denied"}, status=401)
 
     def update(self, request, *args, **kwargs):
