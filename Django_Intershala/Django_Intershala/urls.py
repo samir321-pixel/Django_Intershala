43c96036b081854e66f20ebc5d6948d0b8b9a800
Samir <Saitwalsamir@gmail.com>
2021-03-23 21:24:27 +0530

    --media file url added

--- Django_Intershala/Django_Intershala/urls.py
+++ Django_Intershala/Django_Intershala/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -43,3 +45,5 @@ urlpatterns = [
     path('job_profile/', include('job_profile.urls')),
     path('feedback/', include('feedback.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
