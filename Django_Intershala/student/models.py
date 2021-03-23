a1a4e678aa2fe5a0d755c2be26f65d182b4d7913
Samir <Saitwalsamir@gmail.com>
2021-03-23 21:20:09 +0530

    -- student_register mail logs added

--- Django_Intershala/student/models.py
+++ Django_Intershala/student/models.py
@@ -79,7 +79,9 @@ class StudentNotification(models.Model):
         try:
             send_mail(subject, message, from_email, [email])
         except Exception as e:
-            pass
+            f = open('logs/student/{}.txt'.format(email), 'w+')
+            f.write("Failed to send mail. {}".format(e))
+            f.close()
         StudentNotification.objects.create(student=student,
                                            message="Hello {}, Welcome to Intershala Update your Intershala Profile and Get hired by Recruiters.".format(
                                                student_name))
